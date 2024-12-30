307c29dcd68974f92656317882f1bcc5688f3b97
drexiing <vicmoor07@gmail.com>
2024-12-30 02:03:21 -0300

    Add modmail utilities and support utility configuration files

--- utils/discord/ext/modmail_utils/views.py
+++ utils/discord/ext/modmail_utils/views.py
@@ -79,41 +79,41 @@ class ConfirmView(View):
         self.resolve_label_and_emoji()
 
     def resolve_label_and_emoji(self) -> None:
-        _, accept_emoji, accept_style = self._retrieve_label_and_emoji(boolean=True)
-        self.accept_button.label = None  # No label
+        accept_label, accept_emoji, accept_style = self._retrieve_label_and_emoji(boolean=True)
+        if _validate_button_output(accept_emoji, accept_label):
+            self.accept_button.label = accept_label
         self.accept_button.emoji = accept_emoji
         self.accept_button.style = accept_style
 
-        _, deny_emoji, deny_style = self._retrieve_label_and_emoji(boolean=False)
-        self.deny_button.label = None  # No label
+        deny_label, deny_emoji, deny_style = self._retrieve_label_and_emoji(boolean=False)
+        if _validate_button_output(deny_emoji, deny_label):
+            self.deny_button.label = deny_label
         self.deny_button.emoji = deny_emoji
         self.deny_button.style = deny_style
 
     def _retrieve_label_and_emoji(
-        self,
-        *,
-        boolean: Optional[bool] = None,
-        default_emoji_accept: Optional[str] = "✅",
-        default_emoji_deny: Optional[str] = "❌",
-        default_style_accept: ButtonStyle = ButtonStyle.green,
-        default_style_deny: ButtonStyle = ButtonStyle.red,
+        self, *, boolean: Optional[bool] = None
     ) -> Tuple[Optional[str], Optional[Union[discord.PartialEmoji, discord.Emoji, str]], ButtonStyle]:
         if boolean is None:
             raise ValueError("boolean must be set to True or False.")
 
-        label = None
-        emoji = default_emoji_accept if boolean else default_emoji_deny
-        style = default_style_accept if boolean else default_style_deny
-
         cog = self.bot.get_cog("Extended Utils")
-        if cog is not None:
-            attrs = ["emoji", "style"]
-            config_emoji, config_style = (
-                cog.config.get(f"confirm_button_{'accept' if boolean else 'deny'}_{attr}") for attr in attrs
-            )
+        if cog is None:
+            return
+        attrs = ["label", "emoji", "style"]
+        label, emoji, style = (
+            cog.config.get(f"confirm_button_{'accept' if boolean else 'deny'}_{attr}") for attr in attrs
+        )
 
-            emoji = config_emoji or emoji
-            style = config_style or style
+        if emoji is not None:
+            try:
+                # TODO: Use custom one in utils
+                emoji = _convert_emoji(emoji)
+            except ValueError:
+                emoji = None
+            except EmojiNotFound:
+                # custom emoji not found
+                raise ValueError(f'Emoji "{emoji}" not found.')
 
         return label, emoji, style
 
@@ -125,20 +125,20 @@ class ConfirmView(View):
         await interaction.response.send_message("These buttons cannot be controlled by you.", ephemeral=True)
         return False
 
-    @discord.ui.button(emoji="✅", style=ButtonStyle.green)
+    @discord.ui.button(label="Yes", style=ButtonStyle.green)
     async def accept_button(self, interaction: Interaction, button: discord.ui.Button):
         """
-        Ejecutado cuando el usuario presiona el botón de aceptación.
+        Executed when the user presses the `confirm` button.
         """
         self.interaction = interaction
         self._selected_button = button
         self.value = True
         await self.conclude(interaction)
 
-    @discord.ui.button(emoji="❌", style=ButtonStyle.red)
+    @discord.ui.button(label="No", style=ButtonStyle.red)
     async def deny_button(self, interaction: Interaction, button: discord.ui.Button):
         """
-        Ejecutado cuando el usuario presiona el botón de denegación.
+        Executed when the user presses the `cancel` button.
         """
         self.interaction = interaction
         self._selected_button = button
